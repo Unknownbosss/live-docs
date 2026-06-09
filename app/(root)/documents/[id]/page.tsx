f639b50c3598ec60bf66a5f98761b86182b34c5a
Unknownbosss <udeigweemmanuel0@gmail.com>
2026-06-09 15:56:04 +0100

    Add Document (room) Creation

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 
+import CollaborativeRoom from "@/components/CollaborativeRoom";
 import Editor from "@/components/editor/Editor";
 import Header from "@/components/Header";
-import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import { Show, SignIn, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
 const Document = () => {
   return (
-    <div>
-      
-    </div>
+    <main className="flex w-full flex-col items-center">
+      <CollaborativeRoom />
+    </main>
   );
 };
 
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,51 @@
+import AddDocumentBtn from "@/components/AddDocumentBtn";
+import Header from "@/components/Header";
 import { Button } from "@/components/ui/button";
+import { Show, UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+import { redirect } from "next/navigation";
 
-const Home = () => {
-  return <div>
+const Home = async () => {
+  const clerkUser = await currentUser();
 
-    <Button>Click me</Button>
-  </div>;
+  if (!clerkUser) redirect("/sign-in");
+
+  const documents = [];
+  return (
+    <main className="home-container">
+      <Header className="sticky left-0 top-0">
+        <div className="flex itmes-center gap-2 lg:gap-4">
+          Notification
+          <Show when="signed-in">
+            <UserButton />
+          </Show>
+        </div>
+      </Header>
+
+      <div>
+        {documents.length > 0 ? (
+          <div></div>
+        ) : (
+          <div className="document-list-empty">
+            <Image
+              src="/assets/icons/doc.svg"
+              alt="Document"
+              width={40}
+              height={40}
+              className="mx-auto"
+            />
+            <h1 className="text-2xl font-bold">No documents found</h1>
+
+            <AddDocumentBtn
+              userId={clerkUser.id}
+              email={clerkUser.emailAddresses[0].emailAddress}
+            />
+          </div>
+        )}
+      </div>
+    </main>
+  );
 };
 
 export default Home;
